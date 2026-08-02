d446ccd075da05f09ed97296fe6679381ad84a1c
Mistivia <i@mistivia.com>
2026-08-02 16:26:18 +0800

    remove key lock and use random temp files

--- src/YgoPicProxy.hs
+++ src/YgoPicProxy.hs
@@ -9,12 +9,10 @@ module YgoPicProxy
   ) where
 
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
-import Control.Concurrent.MVar (MVar, modifyMVar, newMVar, withMVar)
 import Control.Exception (IOException, SomeException, catch, try)
 import Control.Monad (forever)
 import qualified Data.ByteString.Lazy as BL
 import Data.Char (isDigit)
-import qualified Data.Map.Strict as Map
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Time.Clock.POSIX (getPOSIXTime)
@@ -23,28 +21,26 @@ import Network.HTTP.Client (Manager, httpLbs, parseRequest, responseBody, respon
 import Network.HTTP.Client.TLS (newTlsManager)
 import Network.HTTP.Types (status200, status404, status500)
 import Network.Wai (Application, Response, ResponseReceived, pathInfo, responseFile, responseLBS)
-import System.Directory (createDirectoryIfMissing, doesFileExist, removeFile, renameFile)
-import System.FilePath (takeFileName, (</>))
+import System.Directory (copyFile, createDirectoryIfMissing, doesFileExist, getTemporaryDirectory, removeFile)
+import System.FilePath ((</>))
+import System.IO (hClose, openTempFile)
 import System.Process (callProcess)
 import Text.Read (readMaybe)
 
 data AppState = AppState
-  { asChan    :: Chan FilePath
+  { asChan    :: Chan (String, FilePath)
   , asDb      :: Connection
-  , asLocks   :: MVar (Map.Map String (MVar ()))
   , asManager :: Manager
   }
 
 newAppState :: IO AppState
 newAppState = do
   createDirectoryIfMissing True "cache"
-  createDirectoryIfMissing True "tmp"
   conn <- open "ygo-pic-proxy.db"
   initDb conn
   mgr <- newTlsManager
   chan <- newChan
-  locks <- newMVar Map.empty
-  pure (AppState chan conn locks mgr)
+  pure (AppState chan conn mgr)
 
 initDb :: Connection -> IO ()
 initDb conn = do
@@ -67,11 +63,7 @@ handleImage st cid respond = do
   cached <- doesFileExist cacheFile
   if cached
     then respond (responseFile status200 [("Content-Type", "image/jpeg")] cacheFile Nothing)
-    else withKeyLock (asLocks st) cid $ do
-      cached' <- doesFileExist cacheFile
-      if cached'
-        then respond (responseFile status200 [("Content-Type", "image/jpeg")] cacheFile Nothing)
-        else fetchMissing
+    else fetchMissing
   where
     fetchMissing :: IO ResponseReceived
     fetchMissing = do
@@ -80,25 +72,38 @@ handleImage st cid respond = do
       case mTs of
         Just ts | now - ts < 3600 -> respond (responseLBS status404 [] "not found")
         _ -> do
-          let webpFile = "tmp" </> (cid ++ ".webp")
+          tmpDir <- getTemporaryDirectory
+          (webpFile, webpHandle) <- openTempFile tmpDir ("ygo-" ++ cid ++ ".webp")
+          hClose webpHandle
           dl <- try (downloadWebp (asManager st) cid webpFile) :: IO (Either SomeException DownloadResult)
           case dl of
-            Left _ -> respond (responseLBS status500 [] "internal error")
+            Left _ -> do
+              removeQuietly webpFile
+              respond (responseLBS status500 [] "internal error")
             Right DownloadNotFound -> do
+              removeQuietly webpFile
               setNotExist (asDb st) cid now
               respond (responseLBS status404 [] "not found")
-            Right DownloadHttpError -> respond (responseLBS status500 [] "internal error")
-            Right DownloadOk -> convertAndServe webpFile
-    convertAndServe :: FilePath -> IO ResponseReceived
-    convertAndServe webpFile = do
-      let jpgFile = "tmp" </> (cid ++ ".jpg")
-      conv <- try (callProcess "magick" [webpFile, jpgFile]) :: IO (Either SomeException ())
-      case conv of
-        Left _ -> respond (responseLBS status500 [] "internal error")
-        Right () -> do
-          img <- BL.readFile jpgFile
-          writeChan (asChan st) jpgFile
-          respond (responseLBS status200 [("Content-Type", "image/jpeg")] img)
+            Right DownloadHttpError -> do
+              removeQuietly webpFile
+              respond (responseLBS status500 [] "internal error")
+            Right DownloadOk -> do
+              (jpgFile, jpgHandle) <- openTempFile tmpDir ("ygo-" ++ cid ++ ".jpg")
+              hClose jpgHandle
+              conv <- try (callProcess "magick" [webpFile, jpgFile]) :: IO (Either SomeException ())
+              removeQuietly webpFile
+              case conv of
+                Left _ -> do
+                  removeQuietly jpgFile
+                  respond (responseLBS status500 [] "internal error")
+                Right () -> do
+                  img <- BL.readFile jpgFile
+                  writeChan (asChan st) (cid, jpgFile)
+                  respond (responseLBS status200 [("Content-Type", "image/jpeg")] img)
+    removeQuietly :: FilePath -> IO ()
+    removeQuietly p = removeFile p `catch` ignoreIO
+    ignoreIO :: IOException -> IO ()
+    ignoreIO _ = pure ()
 
 data DownloadResult = DownloadOk | DownloadNotFound | DownloadHttpError
 
@@ -129,28 +134,17 @@ setNotExist conn cid ts =
 
 worker :: AppState -> IO ()
 worker st = forever $ do
-  tmpJpg <- readChan (asChan st)
-  let name = takeFileName tmpJpg
-      cacheJpg = "cache" </> name
+  (cid, tmpJpg) <- readChan (asChan st)
+  let cacheJpg = "cache" </> (cid ++ ".jpg")
   exists <- doesFileExist cacheJpg
   if exists
     then removeQuietly tmpJpg
-    else renameFile tmpJpg cacheJpg `catch` ignoreIO
+    else copyFile tmpJpg cacheJpg `catch` ignoreIO >> removeQuietly tmpJpg
   where
     removeQuietly :: FilePath -> IO ()
     removeQuietly p = removeFile p `catch` ignoreIO
     ignoreIO :: IOException -> IO ()
     ignoreIO _ = pure ()
-
-withKeyLock :: MVar (Map.Map String (MVar ())) -> String -> IO a -> IO a
-withKeyLock locks key action = do
-  mv <- modifyMVar locks $ \m ->
-    case Map.lookup key m of
-      Just v  -> pure (m, v)
-      Nothing -> do
-        v <- newMVar ()
-        pure (Map.insert key v m, v)
-  withMVar mv $ \_ -> action
 
 parseId :: Text -> Maybe String
 parseId name = do
